e9187a7799fbc011078c6f2c77ac7cbd7337a64c
Akhil Pandey <akhill4054@gmail.com>
2025-11-20 10:22:14 +0530

    Add jitter in local test consumer

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
 
@@ -31,7 +32,7 @@ func main() {
 	}
 
 	wg.Wait()                   // Wait until goroutines are finished running
-	time.Sleep(2 * time.Second) // Wait for subscribes to finish
+	time.Sleep(5 * time.Second) // Wait for subscribes to finish
 
 	fmt.Println("Finished running!")
 }
@@ -47,8 +48,8 @@ func startPublisher(tag string, topic string) {
 		payload := fmt.Sprintf("%s: Message Payload %d", tag, i)
 		msg := encode.Message(broker.Publish, topic, []byte(payload))
 
-		// Write once in every 2 seconds
-		time.Sleep(2 * time.Second)
+		// Write once in every 5 seconds
+		time.Sleep(5 * time.Second)
 		err = conn.WriteMessage(websocket.BinaryMessage, msg)
 		if err != nil {
 			log.Fatal("dial:", err)
@@ -60,6 +61,8 @@ func startPublisher(tag string, topic string) {
 }
 
 func startSubscriber(tag string, topic string) {
+	fmt.Printf("%s: Subscriber started!\n", tag)
+
 	conn, _, err := websocket.DefaultDialer.Dial(ConnectionURL, nil)
 	if err != nil {
 		log.Fatal("dial:", err)
@@ -72,8 +75,15 @@ func startSubscriber(tag string, topic string) {
 		log.Fatal("dial:", err)
 		return
 	}
+	fmt.Printf("%s: Subscribed to %s\n", tag, topic)
 
 	for {
+		// TODO: Fix jitter, does not seem to be working
+		// Random sleep upto 3000ms
+		jitter := rand.Intn(5 * 1000)
+		println("Jitter value:", jitter)
+		time.Sleep(time.Duration(jitter) * time.Millisecond)
+
 		msgType, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Fatal("dial:", err)
